35232612b7d1ba326a84d81900aaf3f43e0f57e3
michaelheyman <mitch.heyman@gmail.com>
2019-08-18 21:50:27 -0700

    Refactor methods and improve area plot

--- pomo-grapher.py
+++ pomo-grapher.py
@@ -59,61 +59,81 @@ def run():
 
     # process data into pandas
     for term, courses in terms.items():
+        plot_courses(courses, term)
 
-        # create and sanitize dataframe structure
-        df = pd.DataFrame.from_dict(
-            courses
-        )  # populate dataframe with courses dictionary
-        df = df.fillna(0)  # fills 'NaN' with zeros
-        df.index = pd.DatetimeIndex(df.index)  # prevents index from being overwritten
-        df = df.reindex(
-            pd.date_range(df.index.min().date(), df.index.max().date()), fill_value=0
-        )  # reindexes
 
-        # create dataframe to plot total minutes plot
-        df2 = df.sum(axis=1)  # sums all data rows into one single row
-        df2 = df2.cumsum()  # adds cumulative sum of column data
+def plot_courses(courses, term):
+    df = create_dataframe(courses)
+    df2 = create_total_minutes_dataframe(df)
 
-        # create array of subplots and figure
-        fig, axes = plt.subplots(nrows=2, ncols=1)
+    # create array of subplots and figure
+    fig, axes = plt.subplots(nrows=2, ncols=1)
 
-        # plot total minutes ax
-        total_minutes_plot = df2.plot.area(ax=axes[0], title=term, figsize=(12, 12))
-        total_minutes_plot.set_ylabel("Total Hours")
-        total_minutes_plot.set_xlim(
-            df2.index.min(), df2.index.max()
-        )  # sets data plotting to origin
+    plot_total_minutes(axes, df2, term)
+    plot_daily_activity(axes, df, term)
 
-        # plot daily activity ax
-        daily_activity_plot = df.plot.bar(
-            ax=axes[1], title=term, figsize=(12, 12), rot=90, stacked=True
-        )
-        daily_activity_plot.set_ylabel("Hours")
+    # display plots
+    fig.savefig(term)  # to file
+    # plt.show()          # on screen
 
-        # remove timestamp from x-axis of daily_activity_plot
-        f = lambda x: datetime.datetime.strptime(x, "%Y-%m-%d %H:%M:%S").strftime(
-            "%b-%d"
-        )
-        daily_activity_plot.set_xticklabels(
-            [f(x.get_text()) for x in daily_activity_plot.get_xticklabels()],
-            visible=False,
-        )
 
-        # display every 7th tick label, to match area plot format
-        for tick in daily_activity_plot.xaxis.get_ticklabels()[::2]:
-            tick.set_visible(True)
+def plot_daily_activity(axes, df, term):
+    daily_activity_plot = df.plot.bar(
+        ax=axes[1], title=term, figsize=(12, 12), rot=90, stacked=True
+    )
+    daily_activity_plot.set_ylabel("Hours")
+    # remove timestamp from x-axis of daily_activity_plot
+    f = lambda x: datetime.datetime.strptime(x, "%Y-%m-%d %H:%M:%S").strftime(
+        "%b-%d"
+    )
+    daily_activity_plot.set_xticklabels(
+        [f(x.get_text()) for x in daily_activity_plot.get_xticklabels()],
+        visible=False,
+    )
+    # display every 7th tick label, to match area plot format
+    for tick in daily_activity_plot.xaxis.get_ticklabels()[::2]:
+        tick.set_visible(True)
+    # plot the average time spent per day
+    daily_activity_plot.axhline(
+        df.values.sum() / len(df.index), color="b", alpha=0.2, ls="dashed"
+    )
+    # adjust horizontal space between subplots
+    plt.subplots_adjust(hspace=0.3)
 
-        # plot the average time spent per day
-        daily_activity_plot.axhline(
-            df.values.sum() / len(df.index), color="b", alpha=0.2, ls="dashed"
-        )
 
-        # adjust horizontal space between subplots
-        plt.subplots_adjust(hspace=0.3)
+def plot_total_minutes(axes, df2, term):
+    # plot total minutes ax
+    total_minutes_plot = df2.plot.area(ax=axes[0], title=term, figsize=(12, 12))
+    total_minutes_plot.set_ylabel("Total Hours")
+    total_minutes_plot.set_xlim(
+        df2.index.min(), df2.index.max()
+    )  # sets data plotting to origin
 
-        # display plots
-        fig.savefig(term)  # to file
-        # plt.show()          # on screen
+
+def create_total_minutes_dataframe(df):
+    """ Create DataFrame to plot total minutes plot """
+
+    columns_dict = {}
+    for column_name in df.columns:
+        columns_dict.update({column_name: df[column_name]})
+
+    total_minutes_df = pd.DataFrame(columns_dict)
+    total_minutes_df.cumsum()
+    return total_minutes_df.cumsum()
+
+
+def create_dataframe(courses):
+    """ Create and sanitize DataFrame structure """
+
+    df = pd.DataFrame.from_dict(
+        courses
+    )  # populate dataframe with courses dictionary
+    df = df.fillna(0)  # fills 'NaN' with zeros
+    df.index = pd.DatetimeIndex(df.index)  # prevents index from being overwritten
+    df = df.reindex(
+        pd.date_range(df.index.min().date(), df.index.max().date()), fill_value=0
+    )  # reindexes
+    return df
 
 
 if __name__ == "__main__":
