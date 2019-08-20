1f560b405866efc8cbf16712d5dea7fb4449f4e0
michaelheyman <mitch.heyman@gmail.com>
2019-08-19 21:18:02 -0700

    Remove code smelly comments and adds globals

--- pomo-grapher.py
+++ pomo-grapher.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 
 FIGURE_WIDTH = 12
 FIGURE_HEIGHT = 12
+HEADER_SYMBOL = "*"
+BEGIN_POMO_LINE = "CLOCK:"
 
 
 def parse_pomodoro_line(line):
@@ -23,42 +25,36 @@ def run():
     with open(filename, "r") as file:
         min_date = datetime.now().date()
         max_date = 0
-        terms = dict()
-        current_term_name = (
-            ""
-        )  # variable set for local access inside course if-statement
-        current_course_name = ""
+        terms = {}
+
+        term_name = None
+        course_name = None
 
         for file_line in file:
             line = file_line.split()
 
-            if "*" in line[0]:
-                if len(line[0]) == 1:  # term
-                    current_term_name = line[1]
-                    terms[current_term_name] = {}
+            if HEADER_SYMBOL in line[0]:
+                if len(line[0]) == 1:
+                    term_name = line[1]
+                    terms[term_name] = {}
 
-                if len(line[0]) == 2:  # course
-                    current_course_name = " ".join(line[1:])
-                    terms[current_term_name].update({current_course_name: {}})
+                if len(line[0]) == 2:
+                    course_name = " ".join(line[1:])
+                    terms[term_name].update({course_name: {}})
 
-                # if len(line[0]) is 3:  # task
-                #    task_name = " ".join(line[1:])
-
-            if line[0] == "CLOCK:":  # pomodoro line
+            if line[0] == BEGIN_POMO_LINE:
                 date, seconds = parse_pomodoro_line(line)
                 if date < str(min_date):
                     min_date = date
                 if date > str(max_date):
                     max_date = date
 
-                seconds = seconds / 60.0  # set seconds to hours
+                hours = seconds / 60.0
 
-                if date in terms[current_term_name][current_course_name].keys():
-                    terms[current_term_name][current_course_name][date] += seconds
+                if date in terms[term_name][course_name].keys():
+                    terms[term_name][course_name][date] += hours
                 else:
-                    terms[current_term_name][current_course_name].update(
-                        {date: seconds}
-                    )
+                    terms[term_name][course_name].update({date: hours})
 
     plot_all_courses(terms)
 
@@ -72,15 +68,15 @@ def plot_courses(courses, term):
     daily_activity_df = create_dataframe(courses)
     cumulative_activity_df = create_total_minutes_dataframe(daily_activity_df)
 
-    # create array of subplots and figure
+    # Create array of subplots and figure
     fig, axes = plt.subplots(nrows=2, ncols=1)
 
     plot_cumulative_activity(axes, cumulative_activity_df, term)
     plot_daily_activity(axes, daily_activity_df, term)
 
-    # display plots
-    fig.savefig(term)  # to file
-    plt.show()  # on screen
+    # Display plots
+    fig.savefig(term)
+    # plt.show()
 
 
 def plot_daily_activity(axes, df, term):
@@ -99,14 +95,16 @@ def plot_daily_activity(axes, df, term):
 
     daily_activity_plot.set_xticklabels(xticklabels, visible=False)
 
-    # display every 7th tick label, to match area plot format
+    # Display every 7th tick label, to match the format of the cumulative plot
+    # TODO: make this actually work
     for tick in daily_activity_plot.xaxis.get_ticklabels()[::2]:
         tick.set_visible(True)
 
     daily_activity_plot.axhline(
         average_time_per_day(df), color="b", alpha=0.2, ls="dashed"
     )
-    # adjust horizontal space between subplots
+
+    # Adjust horizontal space between subplots
     plt.subplots_adjust(hspace=0.3)
 
 
@@ -118,22 +116,22 @@ def average_time_per_day(dataframe):
     return dataframe.values.sum() / len(dataframe.index)
 
 
-def plot_cumulative_activity(axes, df2, term):
-    total_minutes_plot = df2.plot.area(
+def plot_cumulative_activity(axes, cumulative_df, term):
+    total_minutes_plot = cumulative_df.plot.area(
         ax=axes[0], title=term, figsize=(FIGURE_WIDTH, FIGURE_HEIGHT)
     )
     total_minutes_plot.set_ylabel("Total Hours")
-    total_minutes_plot.set_xlim(
-        df2.index.min(), df2.index.max()
-    )  # sets data plotting to origin
+
+    # Sets data plotting to origin
+    total_minutes_plot.set_xlim(cumulative_df.index.min(), cumulative_df.index.max())
 
 
-def create_total_minutes_dataframe(df):
+def create_total_minutes_dataframe(cumulative_df):
     """ Create DataFrame to plot total minutes plot """
 
     columns_dict = {}
-    for column_name in df.columns:
-        columns_dict.update({column_name: df[column_name]})
+    for column_name in cumulative_df.columns:
+        columns_dict.update({column_name: cumulative_df[column_name]})
 
     total_minutes_df = pd.DataFrame(columns_dict)
     total_minutes_df.cumsum()
@@ -142,13 +140,12 @@ def create_total_minutes_dataframe(df):
 
 def create_dataframe(courses):
     """ Create and sanitize DataFrame structure """
-
-    df = pd.DataFrame.from_dict(courses)  # populate dataframe with courses dictionary
-    df = df.fillna(0)  # fills 'NaN' with zeros
-    df.index = pd.DatetimeIndex(df.index)  # prevents index from being overwritten
+    df = pd.DataFrame.from_dict(courses)
+    df = df.fillna(0)
+    df.index = pd.DatetimeIndex(df.index)  # Prevents index from being overwritten
     df = df.reindex(
         pd.date_range(df.index.min().date(), df.index.max().date()), fill_value=0
-    )  # reindexes
+    )
     return df
 
 
